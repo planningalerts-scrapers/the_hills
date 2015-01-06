6779f59fc18b11d13b879c34769ff296fc19fc5d
Henare Degan <henare.degan@gmail.com>
2015-01-06 13:38:48 +1100

    Council has changed to ePathway so copy Wollongong scraper

--- scraper.rb
+++ scraper.rb
@@ -1,90 +1,103 @@
+require "mechanize"
 require 'scraperwiki'
-require 'mechanize'
 
-def clean_whitespace(a)
-  a.gsub("\r", ' ').gsub("\n", ' ').squeeze(" ").strip
-end
+# This is using the ePathway system.
 
-def get_address(agent, info_url)
-  page = agent.get(info_url)
-  table = page.search('.application-details table').first
-  tr_properties = table.search('tr').find{|t| t.inner_text.strip == "Properties"}
-  # Only record the first address if there are several
-  a = tr_properties.next_sibling.at('a')
-  # It's possible that there are no addresses recorded against an application which makes
-  # it pretty much useless. Ah well...
-  clean_whitespace(a.inner_text) if a
-end
+class WollongongScraper
+  attr_reader :agent
 
-def extract_page_content(agent, page)
-  # They have an online submissions system.
-  comment_url = "http://apps.thehills.nsw.gov.au/external/ERequest.aspx"
+  def initialize
+    @agent = Mechanize.new
+  end
 
-  page.at('table.rgMasterTable').search('tr.rgRow').each do |tr|
-    record = {
-      'info_url' => (page.uri + tr.at('a')['href']).to_s,
-      'council_reference' => tr.search('td')[1].inner_text.strip,
-      'date_received' => Date.strptime(tr.search('td')[2].inner_text.strip, '%d/%m/%Y').to_s,
-      'description' => tr.search('td')[3].inner_text.strip,
-      'date_scraped' => Date.today.to_s,
-      'comment_url' => comment_url,
-    }
-
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      # The address is only shown on the application detail page. Only get it if this is a new record
-      record['address'] = get_address(agent, record['info_url'])
-      # Only save the record if there is actually an address
-      ScraperWiki.save_sqlite(['council_reference'], record) if record['address']
+  def extract_urls_from_page(page)
+    content = page.at('table.ContentPanel')
+    if content
+      content.search('tr')[1..-1].map do |app|
+        (page.uri + app.search('td')[0].at('a')["href"]).to_s
+      end
     else
-      puts "Skipping already saved record " + record['council_reference']
+      []
+    end
+  end
+
+  # The main url for the planning system which can be reached directly without getting a stupid session timed out error
+  def enquiry_url
+    "http://epathway.wollongong.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx"
+  end
+
+  # Returns a list of URLs for all the applications on exhibition
+  def urls
+    # Get the main page and ask for the list of DAs on exhibition
+    page = agent.get(enquiry_url)
+    form = page.forms.first
+    form.radiobuttons[0].click
+    page = form.submit(form.button_with(:value => /Next/))
+
+    page_label = page.at('span#ctl00_MainBodyContent_mPageNumberLabel')
+    if page_label.nil?
+      # If we can't find the label assume there is only one page of results
+      number_of_pages = 1
+    elsif page_label.inner_text =~ /Page \d+ of (\d+)/
+      number_of_pages = $~[1].to_i
+    else
+      raise "Unexpected form for number of pages"
+    end
+    urls = []
+    (1..number_of_pages).each do |page_no|
+      # Don't refetch the first page
+      if page_no > 1
+        page = agent.get("http://epathway.wollongong.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx?PageNumber=#{page_no}")
+      end
+      # Get a list of urls on this page
+      urls += extract_urls_from_page(page)
+    end
+    urls
+  end
+
+  def extract_field(field, label)
+    raise "unexpected form" unless field.search('td')[0].inner_text == label
+    field.search('td')[1].inner_text.strip
+  end
+
+  def applications
+    urls.map do |url|
+      # Get application page with a referrer or we get an error page
+      page = agent.get(url, [], URI.parse(enquiry_url))
+
+      table = page.search('table#ctl00_MainBodyContent_DynamicTable > tr')[0].search('td')[0].search('table')[2]
+
+      date_received = extract_field(table.search('tr')[0], "Lodgement Date")
+      day, month, year = date_received.split("/").map{|s| s.to_i}
+      application_id = extract_field(table.search('tr')[2], "Application Number")
+      description = extract_field(table.search('tr')[3], "Proposal").squeeze(" ").strip
+
+      table = page.search('table#ctl00_MainBodyContent_DynamicTable > tr')[2].search('td')[0].search('table')[2]
+      rows = table.search('tr')[0].search('table > tr')[1..-1]
+      if rows
+        addresses = rows.map do |a|
+          a.search('td')[0].inner_text.strip.gsub('  ', ' ')
+        end
+      else
+        addresses = []
+      end
+      record = {
+        "date_received" => Date.new(year, month, day).to_s,
+        "council_reference" => application_id,
+        "description" => description,
+        "address" => addresses[0],
+        "info_url" => enquiry_url,
+        "comment_url" => enquiry_url,
+        "date_scraped" => Date.today.to_s
+      }
+      #p record
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        puts "Skipping already saved record " + record['council_reference']
+      end
     end
   end
 end
 
-# This is the javascript in the page:
-#
-# var theForm = document.forms['aspnetForm'];
-# if (!theForm) {
-#     theForm = document.aspnetForm;
-# }
-# function __doPostBack(eventTarget, eventArgument) {
-#     if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
-#         theForm.__EVENTTARGET.value = eventTarget;
-#         theForm.__EVENTARGUMENT.value = eventArgument;
-#         theForm.submit();
-#     }
-# }
-
-# Simulate javascript asp.net postback
-def doPostBack(page, button)
-  unless button["onclick"] =~ /return false/
-    m = button["onclick"].match(/javascript:__doPostBack\('(.*)','(.*)'\)/)
-    eventTarget = m[1]
-    eventArgument = m[2]
-    form = page.form_with(:name => "aspnetForm")
-    form["__EVENTTARGET"] = eventTarget
-    form["__EVENTARGUMENT"] = eventArgument
-    form.submit
-  end
-end
-
-def do_search(range)
-  agent = Mechanize.new
-
-  url = "http://apps.thehills.nsw.gov.au/DATracking/Modules/ApplicationMaster/default.aspx?page=found&1=#{range}"
-
-  page = agent.get(url)
-  form = page.forms.first
-  page = form.submit(form.button_with(:value => "I Agree"))
-
-  begin
-    extract_page_content(agent, page)
-    # Click the next button
-    next_button = page.at('.rgPageNext')
-    page = doPostBack(page, next_button)
-  end until page.nil? 
-end
-
-
-do_search("thismonth")
-do_search("lastmonth")
+WollongongScraper.new.applications
