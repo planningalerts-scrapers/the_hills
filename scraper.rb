2c1f5ec21186506520a77b55f0bfa0da1a165946
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 12:22:54 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require "epathway_scraper"
 
-EpathwayScraper::Scraper.scrape_and_save(
+EpathwayScraper.scrape_and_save(
   "https://epathway.thehills.nsw.gov.au/ePathway/Production",
-  list_type: :last_30_days, with_gets: true
+  list_type: :last_30_days
 )
